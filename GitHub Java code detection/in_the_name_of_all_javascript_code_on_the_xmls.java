fabb91fa41bcdddcc8a5a68dfccf280bd4ff385d
Sorry No Name <40151365+DADi590@users.noreply.github.com>
2019-10-08 19:54:16 +0100

    Update in_the_name_of_all_javascript_code_on_the_xmls.java

--- GitHub Java code detection/in_the_name_of_all_javascript_code_on_the_xmls.java
+++ GitHub Java code detection/in_the_name_of_all_javascript_code_on_the_xmls.java
@@ -1,1 +1,1 @@
-File to enable GitHub Java detection, since Java code is included inside ZIP files.
+File to enable GitHub JavaScript code detection, since JavaScript code is included inside some XML files and that was the main thing I updated/improved.
